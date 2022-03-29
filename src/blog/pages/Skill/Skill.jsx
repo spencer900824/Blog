959ebc2022bf3183667f0380c591019bdecc2d7c
spencer900824 <spencer900824@gmail.com>
2022-03-29 10:35:24 +0800

    Finish Skill page.

--- src/blog/pages/Skill/Skill.jsx
+++ src/blog/pages/Skill/Skill.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import MainFeaturedPost from "./_components/MainFeaturedPost";
+import Main from './_components/Main';
 
-const Skill = ()=>{
-    return <div>Comming soon</div>
+import skill from "./skill.md"
+
+const mainFeaturedPost = {
+    title: "Skill",
+    description: "What I'm capable of.",
+    image: "https://www.scu.edu/media/institutional-pages/news-amp-events/binarycode760x480.jpg",
+    imageText: "main image description",
+    linkText: "",
+  };
+
+const Skill = () => {
+  return (
+  <div>
+      <MainFeaturedPost post={mainFeaturedPost}/>
+      <Main title="" posts={[skill]}/>
+  </div>
+  );
 };
 
-export default Skill
+export default Skill;
