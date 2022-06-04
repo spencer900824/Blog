dafcebcb52def9fc77a997facfa2f32ce13d9aa8
spencer900824 <spencer900824@gmail.com>
2022-06-05 00:47:49 +0800

    [Habit] Basic architecture of Habit page.

--- src/blog/pages/Habit/Habit.jsx
+++ src/blog/pages/Habit/Habit.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+// IMPORT COMPONENTS
+import Main from "../_components/Main";
+import MainFeaturedPost from "../_components/MainFeaturedPost";
+// IMPORT MD
 
+
+const mainFeaturedPostConf = {
+    title: "My taste",
+    description: "And my life.",
+    image: "https://source.unsplash.com/random",
+    imageText: "main image description",
+    linkText: "",
+  };
 const Habit = ()=>{
-    return <div>Comming soon</div>
+    return(
+        <React.Fragment>
+            <MainFeaturedPost post={mainFeaturedPostConf}/>
+            <Main title="Movie"></Main>
+            <Main title="Music"></Main>
+            <Main title="Workout"></Main>
+
+        </React.Fragment>
+    )
 };
 
 export default Habit
